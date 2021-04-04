8b45531b26cb5872f2efc7df50423231429d97a6
Neshe <neshe.imamoglu@gmail.com>
2021-04-04 15:42:22 +0200

    ilk surum

---
+++ TestClass.java
@@ -0,0 +1,10 @@
+package mygitpackage;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		System.out.println("Version 1");
+		System.out.println("Git' den Merhaba");
+	}
+
+}
