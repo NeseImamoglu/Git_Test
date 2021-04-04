ba13def819d9a88888f3d80875f8edf54dfcbdf6
Nese Imamoglu <81888846+NeseImamoglu@users.noreply.github.com>
2021-04-04 17:13:13 +0200

    Update TestClass.java
    Github.com daki yeni versiyonumu olusturuyorum

--- TestClass.java
+++ TestClass.java
@@ -5,7 +5,8 @@ public class TestClass {
 	public static void main(String[] args) {
 		System.out.println("Version 1");
 		System.out.println("Git' den Merhaba");
-		System.out.println("Github' tan Merhaba");
+		System.out.println("Github' tan Merhaba");// Bu benim Github.com daki yeni kodum
+		System.out.println("Uzak sunucudaki kodum");// Bu benim Github.com daki yeni kodum
 	}
 
 }
