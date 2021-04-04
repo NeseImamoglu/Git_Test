c504820cbee1c48b615ea77caf822e2065ff091f
NeseImamoglu <81888846+NeseImamoglu@users.noreply.github.com>
2021-04-04 15:47:50 +0200

    Update TestClass.java

--- TestClass.java
+++ TestClass.java
@@ -5,6 +5,7 @@ public class TestClass {
 	public static void main(String[] args) {
 		System.out.println("Version 1");
 		System.out.println("Git' den Merhaba");
+		System.out.println("Github' tan Merhaba");
 	}
 
 }
