c3213b216ef6af45adbe592609ee5e63fe4885c7
Neshe <neshe.imamoglu@gmail.com>
2021-04-10 17:49:23 +0200

    nese branchi ilk commitim

--- src/mygitpackage/TestClass.java
+++ src/mygitpackage/TestClass.java
@@ -5,6 +5,14 @@ public class TestClass {
 	public static void main(String[] args) {
 		System.out.println("Version 1");
 		System.out.println("Git' den Merhaba");
+		System.out.println("Github' tan Merhaba");// Bu benim Github.com daki yeni kodum
+		System.out.println("Uzak sunucudaki kodum");// Bu benim Github.com daki yeni kodum
+		System.out.println("10. SATIR bilgisayarim");// Bu benim Github.com daki yeni kodum
+		
+		
+		System.out.println("13. satir bilgisayarim");// Bu benim Github.com daki yeni kodum
+		
+		System.out.println("NESE BRANCHINDE YAPTIGIM KODLAR");
 	}
 
 }
